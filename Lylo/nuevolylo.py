b0eddf4c867cb71d98e6a018e9eb114b2dfb2bf8
Lylo <85044837+RayWolf-0@users.noreply.github.com>
2024-04-09 20:08:51 -0400

    proyectos python
    aqui solamente estoy estudiando mis codigos de python

---
+++ Lylo/nuevolylo.py
@@ -0,0 +1,6 @@
+#Esto es un comentario... 
+x = int(input("Ingrese un valor para x: "))
+y = int(input("ingrese un valor para y: "))
+z = ((1 + x / (y + x) / (1+x))/ y + 1)**3
+print("El valor de z es: " , z)
+#Este fue un calculo matemático... 
